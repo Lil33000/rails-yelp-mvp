8d4fb38a2381843eb44703e2ee5f008d25b9653a
lilian chabaud <lilian.chbd@gmail.com>
2023-11-15 16:48:56 +0100

    yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Restaurant.destroy_all
+
+restaurant_1 = Restaurant.create(name: "Madonna'me 1", address: "cours balguerie Stutenberg 1", phone_number: "01234 578990", category: "chinese")
+restaurant_2 = Restaurant.create(name: "Madonna'me 2", address: "cours balguerie Stutenberg 2", phone_number: "01234 840200", category: "italian")
+restaurant_3 = Restaurant.create(name: "Madonna'me 3", address: "cours balguerie Stutenberg 3", phone_number: "01234 283648", category: "japanese")
+restaurant_4 = Restaurant.create(name: "Madonna'me 4", address: "cours balguerie Stutenberg 4", phone_number: "01234 627394", category: "french")
+restaurant_5 = Restaurant.create(name: "Madonna'me 5", address: "cours balguerie Stutenberg 5", phone_number: "01234 987123", category: "belgian")
